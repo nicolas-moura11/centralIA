e054ee7cfe127a58d64415885782fa209783c047
nicolas-moura11 <nicolasmoura36@gmail.com>
2025-05-02 23:24:26 -0300

    add header

--- src/components/communityList.jsx
+++ src/components/communityList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { fetchCommunities } from "../services/communityService.js"; // se estiver fora de /components
+import { fetchCommunities } from "../services/communityService.js"; 
 import CommunityCard from "./communityCard.jsx";
 
 function CommunityList() {
@@ -15,7 +15,7 @@ function CommunityList() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
       {communities.length > 0 ? (
         communities.map((community) => (
           <CommunityCard key={community.id} community={community} />
